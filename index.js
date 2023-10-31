7cf5af736f62a37e6c1874605d634db548efec49
Tiffany Man <tiffanyman19@gmail.com>
2023-10-31 14:26:41 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,51 @@
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1000,151))
+
+function subtract(a, b){
+    return a - b;
+}
+console.log(add(0,76))
+
+
+function multiply(a,b){
+    return a * b
+}
+console.log(add(58547,2))
+
+
+
+function divide(a,b){
+    return a / b
+}
+console.log(add(0.1,2))
+
+
+function increment(a){
+    
+  return ++a
+}
+console.log(add(1))
+
+function decrement(a){
+    
+    return --a
+  }
+  console.log(add(1))
+
+function makeInt(n){
+    parseInt('45', 10)
+}
+
+function makeInt(string){
+    return parseInt(string,10)
+  }
+
+function preserveDecimal(n){
+    parseFloat('2.22')
+}
+
+function preserveDecimal(string){
+    return parseFloat(string)
+  }
